e73dd1424c2600cbdeccc479e43da680800848b3
SnehaV6 <sneha.varshini@bca.christuniversity.in>
2024-03-08 18:43:15 +0530

    Initial commit

---
+++ Skincare-Recommendation-Android-Application/Skincare/app/src/main/java/com/skincare/retrofit/RetrofitClient.java
@@ -0,0 +1,40 @@
+package com.skincare.retrofit;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+
+//    private static final String BASE_URL = "https://skincare-reccommendation.onrender.com/";
+    private static final String BASE_URL = "http://192.168.29.247:5000/";
+    private static RetrofitClient mInstance;
+    private final Retrofit retrofit;
+
+    public RetrofitClient() {
+
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder()
+                .connectTimeout(30, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .writeTimeout(30, TimeUnit.SECONDS);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+    }
+
+    public static synchronized RetrofitClient getInstance() {
+        if (mInstance == null) {
+            mInstance = new RetrofitClient();
+        }
+        return mInstance;
+    }
+
+    public Api getApi() {
+        return retrofit.create(Api.class);
+    }
+}
